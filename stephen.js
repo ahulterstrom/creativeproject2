d4117c6a9d01b4ac93bf83eb9b98dc7cd54bdc87
Stephen Howard <stephenhoward330@gmail.com>
2019-10-10 03:51:01 +0000

    committin' changes

--- stephen.js
+++ stephen.js
@@ -4,6 +4,32 @@ var div = document.getElementById("mainframe");
 var deck_id;
 
 function StephenFunction() {
+    let html = "";
+    html += "<input type='button' id='stephenbutton' value='New Sorted Deck' onclick='newSortedDeck()'> ";
+    html += "<input type='button' id='stephenbutton' value='New Shuffled Deck' onclick='newShuffledDeck()'> ";
+    html += "<hr>";
+    document.getElementById("mainframe").innerHTML = html;
+}
+
+function newSortedDeck() {
+    myurl = "https://deckofcardsapi.com/api/deck/new/";
+    fetch(myurl)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        console.log(json);
+        deck_id = json.deck_id;
+    });
+    let html = "";
+    html += "<input type='button' id='stephenbutton' value='New Sorted Deck' onclick='newSortedDeck()'> ";
+    html += "<input type='button' id='stephenbutton' value='New Shuffled Deck' onclick='newShuffledDeck()'> ";
+    html += "<input type='button' id='stephenbutton' value='Next Card' onclick='nextCard()'> ";
+    html += "<h1>Sorted Deck:</h1>";
+    html += "<hr>";
+    document.getElementById("mainframe").innerHTML = html;
+}
+
+function newShuffledDeck() {
     myurl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
     fetch(myurl)
     .then(function(response) {
@@ -12,9 +38,11 @@ function StephenFunction() {
         console.log(json);
         deck_id = json.deck_id;
     });
-    let html = "<p>Stephen</p>";
+    let html = "";
+    html += "<input type='button' id='stephenbutton' value='New Sorted Deck' onclick='newSortedDeck()'> ";
+    html += "<input type='button' id='stephenbutton' value='New Shuffled Deck' onclick='newShuffledDeck()'> ";
     html += "<input type='button' id='stephenbutton' value='Next Card' onclick='nextCard()'> ";
-    html += "<input type='button' id='stephenbutton' value='New Deck' onclick='StephenFunction()'> ";
+    html += "<h1>Shuffled Deck:</h1>";
     html += "<hr>";
     document.getElementById("mainframe").innerHTML = html;
 }
