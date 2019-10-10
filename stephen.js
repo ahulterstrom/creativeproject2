7dc286fc007a7e800787184ecdd75c071337a29b
Stephen Howard <stephenhoward330@gmail.com>
2019-10-10 17:15:21 +0000

    Stephen changes

--- stephen.js
+++ stephen.js
@@ -2,8 +2,11 @@
 var myurl;
 var div = document.getElementById("mainframe");
 var deck_id;
+var num_cards;
+var num_drawn;
 
 function StephenFunction() {
+    num_cards = 1;
     let html = "";
     html += "<input type='button' id='stephenbutton' value='New Sorted Deck' onclick='newSortedDeck()'> ";
     html += "<input type='button' id='stephenbutton' value='New Shuffled Deck' onclick='newShuffledDeck()'> ";
@@ -12,6 +15,7 @@ function StephenFunction() {
 }
 
 function newSortedDeck() {
+    num_drawn = 0;
     myurl = "https://deckofcardsapi.com/api/deck/new/";
     fetch(myurl)
         .then(function(response) {
@@ -23,13 +27,18 @@ function newSortedDeck() {
     let html = "";
     html += "<input type='button' id='stephenbutton' value='New Sorted Deck' onclick='newSortedDeck()'> ";
     html += "<input type='button' id='stephenbutton' value='New Shuffled Deck' onclick='newShuffledDeck()'> ";
-    html += "<input type='button' id='stephenbutton' value='Next Card' onclick='nextCard()'> ";
-    html += "<h1>Sorted Deck:</h1>";
+    html += "<input type='button' id='stephenbutton' value='Next Card(s)' onclick='nextCard()'> <br> <br> ";
+    html += "<form>";
+    html += "<label>Number of cards to draw: </label>";
+    html += "<input id='numInput' type='text' value='1'></input> ";
+    html += "</form>";
+    html += "<h1>Sorted Deck: </h1>";
     html += "<hr>";
     document.getElementById("mainframe").innerHTML = html;
 }
 
 function newShuffledDeck() {
+    num_drawn = 0;
     myurl = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
     fetch(myurl)
         .then(function(response) {
@@ -41,22 +50,43 @@ function newShuffledDeck() {
     let html = "";
     html += "<input type='button' id='stephenbutton' value='New Sorted Deck' onclick='newSortedDeck()'> ";
     html += "<input type='button' id='stephenbutton' value='New Shuffled Deck' onclick='newShuffledDeck()'> ";
-    html += "<input type='button' id='stephenbutton' value='Next Card' onclick='nextCard()'> ";
-    html += "<h1>Shuffled Deck:</h1>";
+    html += "<input type='button' id='stephenbutton' value='Next Card(s)' onclick='nextCard()'> <br> <br> ";
+    html += "<form>";
+    html += "<label>Number of cards to draw: </label>";
+    html += "<input id='numInput' type='text' value='1'></input> ";
+    html += "</form>";
+    html += "<h1>Shuffled Deck: </h1>";
     html += "<hr>";
     document.getElementById("mainframe").innerHTML = html;
 }
 
 function nextCard() {
-    myurl = "https://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=1";
+    changeNum();
+    myurl = "https://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=" + num_cards;
     fetch(myurl)
         .then(function(response) {
             return response.json();
         }).then(function(json) {
             console.log(json);
 
-            var newImg = document.createElement("img");
-            newImg.src = json["cards"][0].image;
-            div.appendChild(newImg);
+            for (let i = 0; i < num_cards; i++) {
+                num_drawn += 1;
+                if (num_drawn >= 53) return;
+                var newImg = document.createElement("img");
+                newImg.src = json["cards"][i].image;
+                newImg.style = "margin:20px";
+                div.appendChild(newImg);
+            }
         });
+}
+
+function changeNum() {
+    event.preventDefault();
+    const value = document.getElementById("numInput").value;
+    if (value === "") {
+        return;
+    }
+    else if (!isNaN(value)) {
+        num_cards = value;
+    }
 }
