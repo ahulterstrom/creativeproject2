5f78768885709eb4b16736867a5e750a27eb04a3
SomeDonk <jswense2@byu.edu>
2019-10-10 02:34:35 +0000

    jard funct

--- jared.js
+++ jared.js
@@ -1,3 +1,16 @@
-function JaredFunction(){
-    document.getElementById("mainframe").innerHTML = "<p>Jard</p>";
+function JaredFunction() {
+    var div = document.getElementById("mainframe");
+
+    var myurl = "https://deckofcardsapi.com/api/deck/new/draw/?count=1";
+    console.log(myurl);
+    fetch(myurl, { mode: 'cors' })
+        .then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            console.log(json);
+            
+            var newImg = document.createElement("img");
+            newImg.src = json["cards"][0].image;
+            div.appendChild(newImg);
+        });
 }
