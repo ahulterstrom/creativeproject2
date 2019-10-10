897e1206b6332bd03949dee52a8086c2490696a7
SomeDonk <jswense2@byu.edu>
2019-10-10 03:31:14 +0000

    displays full deck

--- jared.js
+++ jared.js
@@ -1,10 +1,11 @@
 function JaredFunction()
 {
     var div = document.getElementById("mainframe");
+    div.innerHTML = "";
     var deckID = 0;
     var remaining = 0;
 
-    var myurl = "https://deckofcardsapi.com/api/deck/new/draw/?count=1";
+    var myurl = "https://deckofcardsapi.com/api/deck/new/draw/?count=52";
     console.log(myurl);
     fetch(myurl)
         .then(function(response)
@@ -14,41 +15,14 @@ function JaredFunction()
         {
             console.log(json);
 
-            deckID = json["deck_id"];
-            remaining = json["remaining"];
-            var newImg = document.createElement("img");
-            newImg.src = json["cards"][0].image;
-            div.appendChild(newImg);
-        });
-
-    while (remaining > 0)
-    {
-        sleep(1000);
-        var myurl = "https://deckofcardsapi.com/api/<<" + deckID + ">>/new/draw/?count=1";
-        console.log(myurl);
-        fetch(myurl)
-            .then(function(response)
+            for (let i = 0; i < 52; i++)
             {
-                return response.json();
-            }).then(function(json)
-            {
-                console.log(json);
-
-                remaining = json["remaining"];
-
                 var newImg = document.createElement("img");
-                newImg.src = json["cards"][0].image;
+                newImg.src = json["cards"][i].image;
+                newImg.style = "margin:50px";
                 div.appendChild(newImg);
-            });
+            }
 
-        var newtest = document.createElement("p")
-        newtest.innerHTML = "test";
-        div.appendChild(newtest);
-        remaining--;
-    }
-}
-
-function sleep(ms)
-{
-    return new Promise(resolve => setTimeout(resolve, ms));
+            console.log(remaining);
+        });
 }
