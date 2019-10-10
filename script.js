5e05893b78c231460c3d223403e525080c6d6fe8
ahulterstrom <andrewhulterstrom@gmail.com>
2019-10-10 02:29:01 +0000

    added to AndrewFunction

--- script.js
+++ script.js
@@ -16,7 +16,30 @@ function Reset(){
 Reset();
 
 function AndrewFunction(){
-    document.getElementById("mainframe").innerHTML = "<p>Andrew</p>";
+    let inner 
+    // document.getElementById("mainframe").innerHTML = "<p>Andrew</p>";
+    
+    
+    
+    var node = document.getElementById('mainframe');
+    var newDiv = document.createElement('div');
+    newDiv.id = ("pokedexitem" + iCount);
+    newDiv.className = "pokedexitem";
+    newDiv.onclick = function () { pokemonClicked(id) };
+    var newDivImg = document.createElement('img');
+    newDivImg.id = ("pokesprite" + iCount);
+    newDivImg.className = "pokesprite";
+    newDivImg.src = "";
+    var newDivImgBack = document.createElement('img');
+    newDivImgBack.id = ("pokespriteback" + iCount);
+    newDivImgBack.className = "hidden";
+    newDivImgBack.src = "";
+    var newDivPname = document.createElement('p');
+    newDivPname.id = ("pokename" + iCount);
+    newDivPname.className = "pokename";
+    newDiv.append(newDivImg, newDivPname, newDivImgBack);
+    node.appendChild(newDiv);
+    
 }
 
 function JaredFunction(){
