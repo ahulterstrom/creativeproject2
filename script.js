65dfeab375dd4e3f2eaef0db072244b4bf2a17ee
ahulterstrom <andrewhulterstrom@gmail.com>
2019-10-10 02:36:43 +0000

    commented out the fetch in script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var myurl = "https://deckofcardsapi.com/api/deck/new/draw/?count=1";
-console.log(myurl);
-fetch(myurl, { mode: 'cors' })
-    .then(function(response) {
-        return response.json();
-    }).then(function(json) {
-        console.log(json);
-        // document.getElementById("cardimage").src = json["cards"][0].image;
-    });
+// var myurl = "https://deckofcardsapi.com/api/deck/new/draw/?count=1";
+// console.log(myurl);
+// fetch(myurl)
+//     .then(function(response) {
+//         return response.json();
+//     }).then(function(json) {
+//         console.log(json);
+//         document.getElementById("cardimage").src = json["cards"][0].image;
+//     });
 
 
 function Reset(){
@@ -14,5 +14,3 @@ function Reset(){
 }
 
 Reset();
-
-
